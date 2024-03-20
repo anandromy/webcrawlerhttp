33709ccf688fd47708cb63a126fbb29c06dad6a4
anandromy <23f3003270@ds.study.iitm.ac.in>
2024-03-20 15:04:28 +0530

    getURLS from html functions and tests

--- crawl.js
+++ crawl.js
@@ -1,3 +1,5 @@
+const { JSDOM } = require('jsdom')
+
 function normalizeURL(urlString){
     const urlObj = new URL(urlString)
     const hostPath = `${urlObj.hostname}${urlObj.pathname}`
@@ -7,6 +9,35 @@ function normalizeURL(urlString){
     return hostPath
 }
 
+
+function getURLsFROMHTML(htmlBody, baseURL){
+    const urls = []
+    const dom = new JSDOM(htmlBody)
+    const linkElements = dom.window.document.querySelectorAll('a')
+    for (const linkElement of linkElements){
+        if(linkElement.href.slice(0, 1) === "/"){
+            // relative
+            try{
+                const urlObj = new URL(`${baseURL}${linkElement.href}`)
+                urls.push(urlObj.href)
+            }catch(err){
+                console.log(`error with relative url: ${err.message}`)
+            }
+        }else{
+            // absoulte
+            try{
+                const urlObj = new URL(linkElement.href)
+                urls.push(urlObj.href)
+            }catch(err){
+                console.log(`error with absolute url: ${err.message}`)
+            }
+        }
+        
+    }
+    return urls
+}
+
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFROMHTML
 }
